c97983533d1b0be0618970d4527fa7e862e29c53
Afhaam1 <84372124+Afhaam1@users.noreply.github.com>
2022-08-16 11:25:51 +0530

    Add files via upload

---
+++ Bed_Room.js
@@ -0,0 +1,42 @@
+img = ""
+status_1 = ""
+object = []
+function setup(){;
+ canvas = createCanvas(500, 400);
+ canvas.center();
+ objectDetector = ml5.objectDetector('cocossd' , modelLoaded);
+ document.getElementById("status").innerHTML = "Status : Dectecting Objects";
+};
+function modelLoaded(){
+    objectDetector.detect(img ,gotResult);
+    status_1 = true;
+    console.log("modelLoaded")
+}
+function gotResult(error , result){
+    if(error){
+        console.error(error)
+    }console.log(result)
+    object = result
+}
+function draw(){
+    image(img, 0, 0, 500, 400);
+    if( status_1 != ""){
+        for ( i=0;i < object.length ;i++){
+        document.getElementById("status").innerHTML = "status: object detected"
+    fill("red");
+     percent = floor(object[i].confidence*100)
+    text( object[i].label + " " + percent + "%" ,object[i].x + 15 , object[i].y + 15)
+    stroke("red");
+    noFill();
+    rect( object[i].x, object[i].y, object[i].width, object[i].height);
+    }
+    }
+
+}
+function preload(){
+    img = loadImage("bed room.jpg");
+}
+function Back() {
+    window.location = "Index.html"
+    return;
+}
---
+++ Main.js
@@ -0,0 +1,15 @@
+function set_to_bed_room(){
+    window.location = "Bed_Room.html"
+}
+function set_to_TV_and_AC(){
+    window.location = "TV_and_AC.html"
+}
+function set_to_Desk(){
+    window.location = "Desk.html"
+}
+function set_to_Bottles(){
+    window.location = "Bottels.html"
+}
+function set_to_Fruit_Basket(){
+    window.location = "Fruit_Basket.html"
+}
